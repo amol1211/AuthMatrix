40e8c36ed0c57e9af894c42962d19d06638edcb2
Amol Shinde <shindeamol409@gmail.com>
2025-07-09 15:29:17 +0530

    Created new project authmarix

---
+++ AuthMatrix/src/main/java/amol/com/AuthMatrix/AuthMatrixApplication.java
@@ -0,0 +1,13 @@
+package amol.com.AuthMatrix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthMatrixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthMatrixApplication.class, args);
+	}
+
+}
