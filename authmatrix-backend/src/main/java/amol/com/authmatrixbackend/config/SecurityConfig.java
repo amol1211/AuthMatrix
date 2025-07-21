23de2dd6a1d046ab01cc3ef72eb7a5ff8951546b
Amol Shinde <shindeamol409@gmail.com>
2025-07-21 23:00:55 +0530

    removed unneccesary comments

--- authmatrix-backend/src/main/java/amol/com/authmatrixbackend/config/SecurityConfig.java
+++ authmatrix-backend/src/main/java/amol/com/authmatrixbackend/config/SecurityConfig.java
@@ -2,10 +2,7 @@ package amol.com.authmatrixbackend.config;
 
 import org.springframework.web.filter.CorsFilter;
 
-// import amol.com.authmatrixbackend.filter.JwtRequestFilter;
-import amol.com.authmatrixbackend.filter.JwtRequestFilter; // Update this path to the correct package if needed, e.g.:
- // import amol.com.authmatrixbackend.filter.JwtRequestFilter;
- // import amol.com.AuthMatrix.filter.JwtRequestFilter;
+import amol.com.authmatrixbackend.filter.JwtRequestFilter; 
 import amol.com.authmatrixbackend.service.AppUserDetailsService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
