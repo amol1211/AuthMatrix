c81733ad6e63b8e089260591ac59b1eeca8f51d6
Amol Shinde <shindeamol409@gmail.com>
2025-07-24 16:09:27 +0530

    Changed sameSite property to lax.

--- authmatrix-backend/src/main/java/amol/com/authmatrixbackend/controller/AuthController.java
+++ authmatrix-backend/src/main/java/amol/com/authmatrixbackend/controller/AuthController.java
@@ -52,7 +52,7 @@ public class AuthController {
                     .httpOnly(true)
                     .path("/")
                     .maxAge(Duration.ofDays(1)) 
-                    .sameSite("None") // or "Strict" based on your requirements
+                    .sameSite("lax") // or "Strict" based on your requirements
                     .secure(true) 
                     .build();
             return ResponseEntity.ok().header(HttpHeaders.SET_COOKIE, cookie.toString())
